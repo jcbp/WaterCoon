7d87d0341e2ee5bbe686153c8b6ab87b69ef0e27
jcbp <jcbp.jcbp@gmail.com>
2012-08-26 16:51:29 -0300

    Refactoring

--- authentication.php
+++ authentication.php
@@ -5,47 +5,68 @@ include 'classes/DBConnection.php';
 include 'classes/WebResponder.php';
 include 'classes/AppArgs.php';
 
-$arguments = new AppArgs(AppArgs::GET);
+class Auth {
 
-function getResponder() {
-	global $arguments;
-	if ($arguments->isVarExist(Config::JSONP_VAR_NAME)) {
-		$responder = new JSONPResponder();
-		$responder->setFunctionName($arguments->getVar(Config::JSONP_VAR_NAME));
+	private $arguments;
+	private $responder;
+	private $username;
+	private $password;
+	private $queryResult;
+
+	function __construct()
+	{
+		$this->arguments = new AppArgs(AppArgs::GET);
+		$this->getResponder();
+
+		if (!$this->arguments->isVarExist('username', true) || !$this->arguments->isVarExist('password', true)) {
+			$this->responder->respond("Invalid username or password");
+		}
+
+		$this->normalizeStrings();
+		$this->query();
+		$this->authenticate();
 	}
-	else {
-		$responder = new JSONResponder();
+
+	private function getResponder() {
+		if ($this->arguments->isVarExist(Config::JSONP_VAR_NAME)) {
+			$this->responder = new JSONPResponder();
+			$this->responder->setFunctionName($this->arguments->getVar(Config::JSONP_VAR_NAME));
+		}
+		else {
+			$this->responder = new JSONResponder();
+		}
 	}
-	return $responder;
+
+	private function normalizeStrings() {
+		$this->username = $this->arguments->getVar('username'); 
+		$this->password = $this->arguments->getVar('password'); 
+		$this->username = stripslashes($this->username);
+		$this->password = stripslashes($this->password);
+		$this->username = mysql_real_escape_string($this->username);
+		$this->password = mysql_real_escape_string($this->password);
+	}
+	
+	private function query() {
+		$query = "call get_user_by_name('" . $this->username . "')";
+		$dbconn = new DBConnection(Config::DB_CONFIG, $this->responder);
+		$this->queryResult = $dbconn->query($query);
+		$dbconn->close();
+	}
+
+	private function authenticate() {
+		$row = mysqli_fetch_assoc($this->queryResult);
+		if ($row["password"] == $this->password) {
+			session_start();
+			unset($row["password"]);
+			$_SESSION["wc_username"] = $row;
+			$this->responder->respond("success");
+		}
+		else {
+			$this->responder->respond("authentication failure");
+		}
+	}
 }
 
-$responder = getResponder();
-
-if (!$arguments->isVarExist('username', true) || !$arguments->isVarExist('password', true)) {
-	$responder->respond("Invalid username or password");
-}
-
-$username = $arguments->getVar('username'); 
-$password = $arguments->getVar('password'); 
-$username = stripslashes($username);
-$password = stripslashes($password);
-$username = mysql_real_escape_string($username);
-$password = mysql_real_escape_string($password);
-
-$query = "call get_user_by_name('" . $username . "')";
-$dbconn = new DBConnection(Config::DB_CONFIG, $responder);
-$result = $dbconn->query($query);
-$dbconn->close();
-
-$row = mysqli_fetch_assoc($result);
-if ($row["password"] == $password) {
-	session_start();
-	unset($row["password"]);
-	$_SESSION["wc_username"] = $row;
-	$responder->respond("success");
-}
-else {
-	$responder->respond("authentication failure");
-}
+new Auth();
 
 ?>
