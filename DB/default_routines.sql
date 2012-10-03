cc210aca53c9e795e6687f0d938edff1f4205a7c
jcbp <jcbp.jcbp@gmail.com>
2012-10-02 21:42:09 -0300

    Se escapea palabra reservada

--- DB/default_routines.sql
+++ DB/default_routines.sql
@@ -13,7 +13,7 @@ END $$
             
 DROP PROCEDURE IF EXISTS insert_field $$
 
-CREATE PROCEDURE insert_field(IN field_id int(11), IN list_id int(11), IN field_type_id int(3), IN order_index int(3), IN name varchar(128), IN values varchar(256), IN style varchar(256), IN width int(3), IN default_value blob(65535), IN field_timestamp timestamp)
+CREATE PROCEDURE insert_field(IN field_id int(11), IN list_id int(11), IN field_type_id int(3), IN order_index int(3), IN name varchar(128), IN `values` varchar(256), IN style varchar(256), IN width int(3), IN default_value blob(65535), IN field_timestamp timestamp)
 BEGIN
       IF field_id<=0 OR ISNULL(field_id) THEN
             INSERT INTO field (`list_id`, `field_type_id`, `order_index`, `name`, `values`, `style`, `width`, `default_value`, `field_timestamp`)
